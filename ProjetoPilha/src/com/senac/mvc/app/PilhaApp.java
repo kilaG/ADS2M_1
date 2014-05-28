003d1218137b44a319cc30b38806d9f2d183031d
kilaG <guigakila@gmail.com>
2014-05-28 20:19:05 -0300

    trocando CharArray por vetor de String

--- ProjetoPilha/src/com/senac/mvc/app/PilhaApp.java
+++ ProjetoPilha/src/com/senac/mvc/app/PilhaApp.java
@@ -20,7 +20,7 @@ public class PilhaApp {
 		
 		PilhaController pilha = new PilhaController();
 		
-		Pilha palindromo = new Pilha();
+		/*Pilha palindromo = new Pilha();
 		palindromo.push("o");
 		palindromo.push("v");
 		palindromo.push("o");
@@ -30,10 +30,11 @@ public class PilhaApp {
 		Pilha p1 = new Pilha();
 		Pilha p2 = new Pilha();		
 		p1.push(50);		
-		pilha.mostraBinario(p1,p2);
+		pilha.mostraBinario(p1,p2);*/
 		
 	
-		String exp="2*2+(2*2)";
+		//String exp="2*2+(2*2)";
+		String[] exp={"2","*","2","+","(","2","*","2",")"};
 		Pilha p = new Pilha();
 		pilha.postfixCalculator(p, pilha.infixaparaposfixa(p, exp));
 		pilha.mostraExp(p);
--- ProjetoPilha/src/com/senac/mvc/controller/PilhaController.java
+++ ProjetoPilha/src/com/senac/mvc/controller/PilhaController.java
@@ -1,17 +1,19 @@
 package com.senac.mvc.controller;
 
+import java.util.Scanner;
+
 import com.senac.mvc.model.Pilha;
 import com.senac.mvc.view.ExtendView;
 
 public class PilhaController {
 	
 	private ExtendView view = new ExtendView();
-	
+	private Scanner scan = new Scanner(System.in);
 	
 	private Pilha p;	
 
 	
-	// transforma uma expressao infixa para a forma posfixa
+/*	// transforma uma expressao infixa para a forma posfixa
 	public String infixaparaposfixa(Pilha p,String expre){
 		
 		
@@ -139,17 +141,213 @@ public class PilhaController {
 		
 		return posfix;
 	
+	}*/
+	
+	
+	public String[] receberInfixa(){
+		String[] array = new String[20];
+		int a=0;
+		int TamArray2=0;
+		for(int i=0;i<20;i++){
+			System.out.println("digite 1 para adicinar algo na expressao");
+			a=scan.nextInt();
+			if(a==1){
+		//int i=0;
+		
+		System.out.println("insira algo na expressao...");
+		String str="";
+		str=scan.nextLine();
+		array[i]=str;
+		//i++;
+			}else{
+				break;
+			}
+		}
+		
+		for(int ii=0;ii<array.length;ii++){
+			if(array[ii]!=null){
+				TamArray2++;
+			}else{
+				break;
+			}
+		}
+		String[] array2 = new String[TamArray2];
+		
+		for(int aa=0;aa<=TamArray2;aa++){
+			array2[aa]=array[aa];
+		}
+		
+		return array2;
 	}
 	
 	
 	
+	
+	// transforma uma expressao infixa para a forma posfixa
+	public String[] infixaparaposfixa(Pilha p,String[] expre){
+		
+		
+		String[] exp = expre;		
+		//String posfix = "";
+		boolean isover=false;
+		String[] posfix = new String[20];
+		
+		int indicePosfix=0;
+		
+		
+		//percorre a expressao
+		for(int i =0 ; i < exp.length;i++){
+			
+			if(ehNumero(exp[i])){
+				posfix[indicePosfix]+=exp[i];		
+				
+			}else if(isOperator(exp[i])){
+				
+				if(p.isEmpty()){
+					p.push(exp[i]);
+					
+				}else if(ehAbre(exp[i])){
+					p.push(exp[i]);					
+					
+					
+				}else{
+					
+					int ii=p.size();
+					boolean vdd=false;
+					//percorre a pilha enquanto existir elementos nela
+					for(int iii=0;iii<ii;iii++){
+						
+						if(isover){
+							break;
+						}else{
+							
+						}						
+					if(prioridade(p.top().toString().toCharArray())>prioridade(exp[i])){
+						
+						if(p.top().toString().equals("(")){							
+							p.push(exp[i]);								
+							break;
+						}else{						
+							posfix[indicePosfix]+=p.pop().toString();
+						p.push(exp[i]);
+											
+						}
+						
+					}else if(prioridade(p.top().toString().toCharArray())<prioridade(exp[i])){
+						
+						if(ehFecha(exp[i])){						
+							
+							while(!p.isEmpty()){
+								if(p.top().toString().equals("(")){
+									p.pop();
+									isover=true;
+									break;
+								}else{
+									posfix[indicePosfix]+=p.pop();
+								}
+							}
+							
+						}else{					
+						
+						p.push(exp[i]);
+						
+						
+						
+						}
+					}else if(prioridade(p.top().toString().toCharArray())==prioridade(exp[i])){
+						
+						if(p.size()==1){
+							posfix[indicePosfix]+=p.pop();
+							p.push(exp[i]);							
+						}else if(p.size()>1){
+							posfix[indicePosfix]+=p.pop().toString();
+							
+							
+						}						
+						if(iii== (expre.length-1)){
+							while(!p.isEmpty()){
+								posfix[indicePosfix]+=p.pop();
+							}
+						}
+						
+					}else if(isParenFecha(exp[i])){
+						int aa=p.size();
+						
+						for(int a=0;a<aa;a++){
+					
+							if(!p.top().toString().equals("(")){
+							char aux[]=p.pop().toString().toCharArray();
+							posfix[indicePosfix]+=aux[0];
+							
+						}else{					
+							p.pop();
+							
+							
+						}
+						
+						}
+						
+						
+						
+					}
+					
+					
+					
+												}
+					
+					
+					
+				}
+				
+			}
+			
+			if(isover){
+				break;
+			}
+			
+			
+		}
+		
+		while(!p.isEmpty()){
+			posfix[indicePosfix]+=p.pop();
+		}
+		
+		return posfix;
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//calcula expressao posfixa
-	public Pilha postfixCalculator(Pilha p,String pos){
-		char[] exp=pos.toCharArray();
+	public Pilha postfixCalculator(Pilha p,String[] exp){
+	//	char[] exp=pos.toCharArray();
 		
 		for(int i=0;i<exp.length;i++){
+			System.err.println("\n\n"+i);
 			
-			
+			System.out.println("\n\n\nexp= "+exp[i]);
 			if(ehNumero(exp[i])){	
 				
 				p.push(exp[i]);
@@ -171,14 +369,14 @@ public class PilhaController {
 		
 	}
 	
-	public int calculadora(int a, int b, char c){
-		if(c=='*'){
+	public int calculadora(int a, int b, String exp){
+		if(exp.equals("*")){
 			return a*b;
-		}else if(c=='/'){
+		}else if(exp.equals("/")){
 			return b/a;
-		}else if(c=='-'){
+		}else if(exp.equals("-")){
 			return a-b;
-		}else if(c=='+'){
+		}else if(exp.equals("+")){
 			return a+b;
 		}else{
 			return 0;
@@ -194,7 +392,7 @@ public class PilhaController {
 	
 	
 	
-	public void metodoAux(Pilha p, String posfix, char[] exp, int i){
+/*	public void metodoAux(Pilha p, String posfix, char[] exp, int i){
 		if(!p.isEmpty()){
 			int v=p.size();
 			for(int x=0;x<v;x++){
@@ -207,19 +405,19 @@ public class PilhaController {
 				}
 			}
 		}
-	}
+	}*/
 	
 	
-	public boolean isOperator(char c){
-		if(c=='+'||c=='-'||c=='*'||c=='/'||c=='$'||c=='('||c==')'){
+	public boolean isOperator(String exp){
+		if(exp.equals("+")||exp.equals("-")||exp.equals("*")||exp.equals("/")||exp.equals("$")||exp.equals("(")||exp.equals(")")){
 			return true;			
 		}else{
 			return false;
 		}
 	}
 	
-	public boolean isParenFecha(char c){
-		if(c==')'){
+	public boolean isParenFecha(String exp){
+		if(exp.equals(")")){
 			return true;
 		}else{
 			return false;
@@ -228,7 +426,10 @@ public class PilhaController {
 	
 	public boolean ehNumero(Object c){
 		try{
-		String a=c.toString();
+			String a ="";
+		/*String*/ 
+			System.out.println("objeto ="+c.toString());
+			a=c.toString();
 		if(Integer.parseInt(a)>=0||Integer.parseInt(a)<=0){
 			return true;
 		}else{
@@ -242,28 +443,28 @@ public class PilhaController {
 		
 	}
 	
-	public boolean ehAbre(char c){
-		if(c=='('){
+	public boolean ehAbre(String exp){
+		if(exp.equals("(")){
 			return true;
 		}else{
 			return false;
 		}
 	}
 	
-	public boolean ehFecha(char c){
-		if(c==')'){
+	public boolean ehFecha(String exp){
+		if(exp.equals(")")){
 			return true;
 		}else{
 			return false;
 		}
 	}
 	
-	public int prioridade(char c){
-		if(c=='+'||c=='-'){
+	public int prioridade(String exp){
+		if(exp.equals("+")||exp.equals("-")){
 				return 1;		
-		}else if(c=='*'||c=='/'){
+		}else if(exp.equals("*")||exp.equals("/")){
 			return 2;
-		}else if(c=='('){
+		}else if(exp.equals("(")){
 			return 4;
 		}else{
 			return 3;
